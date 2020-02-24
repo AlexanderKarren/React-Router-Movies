9e1acb3790a13b7d2915f6645e22109dbfabf93a
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-02-24 14:20:55 -0700

    consolidated code to make it DRY

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MovieCard = props => {
-  return;
+  const [movie, setMovie] = useState(props.movie);
+
+  // useEffect(() => {
+  //   const id = params.movieID;
+  //   axios
+  //   .get(`http://localhost:5000/api/movies/${id}`)
+  //   .then(response => {
+  //     setMovie(response.data);
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  // },[params.movieID]);
+
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
